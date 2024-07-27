83397d9bcb8dc5af98c192e7a4cb0c2a471124bf
Joe Ortiz <Se_sema_95@hotmail.com>
2024-07-27 13:45:30 +0000

    Initial commit

--- destructuring.js
+++ destructuring.js
@@ -4,9 +4,62 @@
  */
 
 // Destructuring arrays
+let ages = [30, 26, 27];
+let john = ages[0];
+let mary = ages[1];
+let joe = ages[2];
+console.log(john, mary, joe);
+
+let newAges = [40, 36, 37];
+let [juan, maria, jose] = newAges;
+console.log(juan, maria, jose);
 
 // Destructuring objects
+let jobs = {
+    mike: "designer",
+    jill: "developer",
+    alicia: "accountant",
+};
+let {mike, jill, alicia} = jobs;
+console.log(mike, jill, alicia);
 
 // Destructuring subsets
+let languages = ["english", "french", "spanish", "german", "japanese"];
+let [johnNative, johnSecondary] = languages;
+console.log(johnNative, johnSecondary);
+console.log(languages);
+
+let [, , maryNative, marySecondary] = languages; // The commas ensure the first two values in the "languages" array are ignored!
+console.log(maryNative, marySecondary);
+console.log(languages);
+
+let languages2 = {
+    firstLanguage: "english",
+    secondLanguage: "french",
+    thirdLanguage: "spanish",
+    fourthLanguage: "german",
+    fifthLanguage: "japanese",
+};
+let { firstLanguage, thirdLanguage } = languages2;
+console.log(firstLanguage);
+console.log(firstLanguage, thirdLanguage);
+console.log(languages2);
 
 // Using rest parameter syntax
+let fruits = ["apple", "orange", "banana", "peach", "cherry"];
+let [favourite, secondFavourite, ...others] = fruits;
+console.log(favourite);
+console.log(secondFavourite);
+console.log(others);
+
+let favouriteFoods = {
+    brian: "pizza",
+    anna: "pasta",
+    sarah: "vegetarian",
+    andrea: "steak",
+};
+
+let {brian, anna, ...rest} = favouriteFoods;
+console.log(brian);
+console.log(anna);
+console.log(rest);
